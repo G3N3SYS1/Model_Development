013a6488957b37de1705c4c2201133b96148da18
Kai <kaizhe@teleradio.com.sg>
2024-06-03 17:12:14 +0800

    test: image.isbright function

---
+++ tests/test_image.py
@@ -0,0 +1,70 @@
+from contextlib import ExitStack as DoesNotRaise
+
+import numpy as np
+import pytest
+
+import svlm.utils.image as image
+
+
+@pytest.mark.parametrize(
+    "img, contour, label, expected_result, exception",
+    [
+        (
+            np.full((500, 500, 3), 255, dtype=np.uint8),
+            np.array([]),
+            "",
+            None,
+            pytest.raises(AssertionError),
+        ),  # empty contour
+        (
+            np.full((600, 600, 3), 255, dtype=np.uint8),
+            np.array(
+                [
+                    [[555, 276]],
+                    [[554, 277]],
+                    [[554, 277]],
+                    [[554, 278]],
+                    [[554, 279]],
+                    [[554, 281]],
+                    [[554, 282]],
+                    [[554, 284]],
+                    [[553, 285]],
+                    [[553, 286]],
+                    [[552, 287]],
+                    [[552, 288]],
+                    [[551, 288]],
+                    [[551, 306]],
+                    [[552, 307]],
+                    [[553, 307]],
+                    [[559, 300]],
+                    [[560, 300]],
+                    [[564, 297]],
+                    [[565, 297]],
+                    [[570, 291]],
+                    [[570, 290]],
+                    [[571, 289]],
+                    [[571, 288]],
+                    [[569, 286]],
+                    [[569, 284]],
+                    [[568, 283]],
+                    [[568, 277]],
+                    [[566, 276]],
+                ]
+            ),
+            "",
+            True,
+            DoesNotRaise(),
+        ),  # one contour
+    ],
+)
+def test_isbright(
+    img: np.ndarray,
+    contour: np.ndarray,
+    label: str,
+    expected_result: bool,
+    exception: Exception,
+) -> None:
+
+    with exception:
+        result = image.isbright(img, contour, label)
+        assert result == expected_result
